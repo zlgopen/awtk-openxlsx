ca78fc336b8c44b1553be83d1e1872a36121e1a7
lixianjing <xianjimli@hotmail.com>
2021-03-07 07:32:40 +0800

    add sample to modify image

--- demos/modify.cpp
+++ demos/modify.cpp
@@ -1,8 +1,35 @@
-#include <OpenXLSX.hpp>
 #include <iostream>
+#include "zippy.hpp"
+#include <OpenXLSX.hpp>
+
+#include "tkc/fs.h"
+#include "tkc/mem.h"
 
 using namespace std;
+using namespace Zippy;
 using namespace OpenXLSX;
+
+static ret_t zip_replace_entry(const char* zip_filename, const char* entry_name, const char* filename) {
+  uint8_t* data = NULL;
+  uint32_t size = 0;
+
+  ZipArchive zip(zip_filename);
+
+  data = (uint8_t*)file_read(filename,  &size);
+  return_value_if_fail(data != NULL, RET_BAD_PARAMS);
+
+  ZipEntryData edata(size);
+  memcpy(edata.data(), data, size);
+  TKMEM_FREE(data);
+
+  zip.DeleteEntry(entry_name);
+  zip.AddEntry(entry_name, edata);
+
+  zip.Save();
+  zip.Close();
+
+  return RET_OK;
+}
 
 int main()
 {
@@ -20,5 +47,7 @@ int main()
 
     doc.saveAs("modified.xlsx");
 
+    zip_replace_entry("modified.xlsx", "xl/media/image2.jpeg", "data/orange.jpeg");
+  
     return 0;
 }
